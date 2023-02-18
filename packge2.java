2bd372c377871400c99d6018c606a33ef80cffbe
danielbruno18 <110574262+danielbruno18@users.noreply.github.com>
2023-02-17 21:41:55 -0300

    Add files via upload

---
+++ packge2.java
@@ -0,0 +1,18 @@
+package packge1;
+
+public class packge2 {
+
+	public static void main(String[] args) {
+		double salarioDoFuncionario = 1300.50;
+        double bonusMensal = 250.00; 
+        double horas = 2.0;
+        
+        
+        double saldoDeConta = (salarioDoFuncionario + bonusMensal)*horas;
+        
+        
+        System.out.println("O saldo do funcionario é:" + saldoDeConta);
+        
+	}
+
+}
